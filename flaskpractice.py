6db4946abf549606c3b19398654d0a09aa9454d5
Armand Syahtama <Armand.Syahtama@gmail.com>
2017-08-27 09:50:02 -0400

    Added form field on index

--- flaskpractice.py
+++ flaskpractice.py
@@ -2,19 +2,33 @@ from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 from datetime import datetime
 
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard and obscure string'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<name>', methods=['GET'])
