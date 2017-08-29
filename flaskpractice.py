e862cda24b5f145169f760cdfccb10ebd4415c8d
Armand Syahtama <Armand.Syahtama@gmail.com>
2017-08-28 21:04:24 -0400

    Added email

--- flaskpractice.py
+++ flaskpractice.py
@@ -1,4 +1,5 @@
 import os
+from threading import Thread
 
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager, Shell
@@ -31,7 +32,9 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
-app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+app.config['FLASKY_MAIL_SENDER'] = os.environ.get('MAIL_USERNAME')
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+
 
 mail = Mail(app)
 db = SQLAlchemy(app)
@@ -48,13 +51,20 @@ def make_shell_context():
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender=app.config['FLASKY_MAIL_SENDER'],
                   recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
+    print("life")
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
-
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -86,6 +96,8 @@ def hello_world():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
